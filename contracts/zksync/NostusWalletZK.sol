bad4522f74497ec92dd519237fba7febce18485f
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2023-08-17 18:18:57 -0300

    Create Notus Wallet and Factory v1

---
+++ contracts/zksync/NostusWalletZK.sol
@@ -0,0 +1,253 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.17;
+
+import "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
+import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+
+import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract NotusWalletZK is IAccount, IERC1271 {
+    using TransactionHelper for Transaction;
+
+    bytes4 constant EIP1271_SUCCESS_RETURN_VALUE = 0x1626ba7e;
+
+    address public owner;
+
+    modifier onlyBootloader() {
+        require(
+            msg.sender == BOOTLOADER_FORMAL_ADDRESS,
+            "Only bootloader can call this function"
+        );
+        _;
+    }
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function validateTransaction(
+        bytes32,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader returns (bytes4 magic) {
+        magic = _validateTransaction(_suggestedSignedHash, _transaction);
+    }
+
+    function _validateTransaction(
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) internal returns (bytes4 magic) {
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(
+                INonceHolder.incrementMinNonceIfEquals,
+                (_transaction.nonce)
+            )
+        );
+
+        bytes32 txHash;
+        if (_suggestedSignedHash == bytes32(0)) {
+            txHash = _transaction.encodeHash();
+        } else {
+            txHash = _suggestedSignedHash;
+        }
+
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        require(
+            totalRequiredBalance <= address(this).balance,
+            "Not enough balance for fee + value"
+        );
+
+        if (
+            isValidSignature(txHash, _transaction.signature) ==
+            EIP1271_SUCCESS_RETURN_VALUE
+        ) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+    }
+
+    function executeTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _executeTransaction(_transaction);
+    }
+
+    function _executeTransaction(Transaction calldata _transaction) internal {
+        uint256 to = _transaction.to;
+        // By convention, the `reserved[1]` field is msg.value
+        uint256 value = _transaction.reserved[1];
+        bytes memory data = _transaction.data;
+
+        bool success;
+        // execute transaction
+        assembly {
+            success := call(
+                gas(),
+                to,
+                value,
+                add(data, 0x20),
+                mload(data),
+                0,
+                0
+            )
+        }
+
+        require(success);
+    }
+
+    function executeTransactionFromOutside(
+        Transaction calldata _transaction
+    ) external payable {
+        _validateTransaction(bytes32(0), _transaction);
+        _executeTransaction(_transaction);
+    }
+
+    function executeBatchTransaction(
+        bytes[] calldata datas,
+        address caller
+    ) public payable {
+        require(msg.sender == address(this));
+        uint256 length = datas.length;
+        for (uint256 i = 0; i < length; i++) {
+            (bool success, ) = caller.call{value: msg.value}(datas[i]);
+            require(success, "Transaction fail");
+        }
+    }
+
+    function isValidSignature(
+        bytes32 _hash,
+        bytes memory _signature
+    ) public view override returns (bytes4 magic) {
+        magic = EIP1271_SUCCESS_RETURN_VALUE;
+
+        if (_signature.length != 65) {
+            _signature = new bytes(65);
+            _signature[64] = bytes1(uint8(27));
+        }
+
+        bytes memory signature = extractECDSASignature(_signature);
+
+        if (!checkValidECDSASignatureFormat(signature)) {
+            magic = bytes4(0);
+        }
+
+        address recoveredAddr = ECDSA.recover(_hash, signature);
+
+        if (recoveredAddr != owner) {
+            magic = bytes4(0);
+        }
+    }
+
+    function payForTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        bool success = _transaction.payToTheBootloader();
+        require(success, "Failed to pay the fee to the operator");
+    }
+
+    function prepareForPaymaster(
+        bytes32, // _txHash
+        bytes32, // _suggestedSignedHash
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _transaction.processPaymasterInput();
+    }
+
+    // This function verifies that the ECDSA signature is both in correct format and non-malleable
+    function checkValidECDSASignatureFormat(
+        bytes memory _signature
+    ) internal pure returns (bool) {
+        if (_signature.length != 65) {
+            return false;
+        }
+
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        // Signature loading code
+        // we jump 32 (0x20) as the first slot of bytes contains the length
+        // we jump 65 (0x41) per signature
+        // for v we load 32 bytes ending with v (the first 31 come from s) then apply a mask
+        assembly {
+            r := mload(add(_signature, 0x20))
+            s := mload(add(_signature, 0x40))
+            v := and(mload(add(_signature, 0x41)), 0xff)
+        }
+        if (v != 27 && v != 28) {
+            return false;
+        }
+
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (301): 0 < s < secp256k1n ÷ 2 + 1, and for v in (302): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        if (
+            uint256(s) >
+            0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function extractECDSASignature(
+        bytes memory _fullSignature
+    ) internal pure returns (bytes memory signature1) {
+        require(_fullSignature.length == 65, "Invalid length");
+
+        signature1 = new bytes(65);
+
+        // Copying the first signature. Note, that we need an offset of 0x20
+        // since it is where the length of the `_fullSignature` is stored
+        assembly {
+            let r := mload(add(_fullSignature, 0x20))
+            let s := mload(add(_fullSignature, 0x40))
+            let v := and(mload(add(_fullSignature, 0x41)), 0xff)
+
+            mstore(add(signature1, 0x20), r)
+            mstore(add(signature1, 0x40), s)
+            mstore8(add(signature1, 0x60), v)
+        }
+    }
+
+    fallback() external {
+        // fallback of default account shouldn't be called by bootloader under no circumstances
+        assert(msg.sender != BOOTLOADER_FORMAL_ADDRESS);
+
+        // If the contract is called directly, behave like an EOA
+    }
+
+    receive() external payable {
+        // If the contract is called directly, behave like an EOA.
+        // Note, that is okay if the bootloader sends funds with no calldata as it may be used for refunds/operator payments
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,35 +1,44 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@matterlabs/hardhat-zksync-toolbox";
-
-
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
+import "@matterlabs/hardhat-zksync-verify";
+
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://0.0.0.0:8011",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "https://rpc.ankr.com/eth_goerli",
+        zksync: true,
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
   zksolc: {
     version: "latest",
     settings: {
-      isSystem: true,
+      isSystem: true
     },
   },
   defaultNetwork: "zkSyncTestnet",
   networks: {
-    zkSyncTestnet: {
-      url: "https://testnet.era.zksync.dev",
-      ethNetwork: "https://rpc.ankr.com/eth_goerli",
-      zksync: true,
-      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
-    },
     hardhat: {
-      forking: {
-        url: "https://testnet.era.zksync.dev",
-      },
+      zksync: true,
     },
+    zkSyncTestnet,
+  },
+  solidity: {
+    version: "0.8.17",
   },
   mocha: {
     timeout: 100000000
-  },
+  }
 };
 
 export default config;
